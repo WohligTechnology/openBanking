10ec07af29c44458c8c5a3dea83e51b13fb685bb
kishoribihade <kishori.bihade@wohlig.com>
2018-02-03 11:42:26 +0530

    popup

--- www/js/controllers/UserTableCtrl.js
+++ www/js/controllers/UserTableCtrl.js
@@ -1,3 +1,17 @@
-connector.controller('UserTableCtrl', function ($scope) {
+connector.controller('UserTableCtrl', function ($scope,$ionicPopup, $timeout) {
+
+    $scope.showConfirm = function() {
+        var confirmPopup = $ionicPopup.confirm({
+        //   title: 'Consume Ice Cream',
+          template: 'Are you sure you want to  this ice cream?'
+        });
+        confirmPopup.then(function(res) {
+          if(res) {
+            console.log('You are sure');
+          } else {
+            console.log('You are not sure');
+          }
+        });
+      };
 
 });
